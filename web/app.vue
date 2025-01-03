92078b68c2684d3398ccf85478f8ff78362fb847
xnacly <47723417+xNaCly@users.noreply.github.com>
2025-01-04 00:39:43 +0100

    web: setup

---
+++ web/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <UContainer>
+    <UCard class="mt-10">
+      <template #header>
+        <div class="flex justify-between">
+          <h1 class="text-lg">Mahlzeit</h1>
+          <ColorScheme
+            ><USelect
+              v-model="$colorMode.preference"
+              :options="['system', 'light', 'dark']"
+          /></ColorScheme>
+        </div>
+      </template>
+    </UCard>
+  </UContainer>
+</template>
